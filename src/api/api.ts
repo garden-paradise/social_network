028cd4dc6151ebd3651dfa3d039729238208932f
garden-paradise <asswk1875@gmail.com>
2024-10-08 01:57:37 +0300

    recovery

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,11 @@
 import axios from 'axios';
 import { MyFriendsProfileType } from '../redux/app-reducer';
 
-axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
-
 export let instance = axios.create({
   withCredentials: true,
   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
   headers: {
-    // 'API-KEY': 'e9c26744-99a1-4391-9d00-e03a7fab4f78',
+    'API-KEY': 'e9c26744-99a1-4391-9d00-e03a7fab4f78',
   },
 });
 
